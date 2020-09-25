26fcbdeeb9a7e20b228338bc3ec9094918dd8ce7
pjw <jinwen.pu@zenlayer.com>
2020-09-25 16:40:19 +0800

    单链表的基本操作java实现

--- src/main/java/LinkedListDemo/MyLink.java
+++ src/main/java/LinkedListDemo/MyLink.java
@@ -1,5 +1,6 @@
 package LinkedListDemo;
 
+
 /**
  * @ClassName MyLink
  * @Description java实现链表的操作
@@ -51,7 +52,7 @@ public class MyLink {
         Node temp=head.next;
         while (temp!=null){
             if(temp.data!=0){
-                System.out.println("链表的数据"+temp.data);
+                System.out.println("链表的数据 {"+temp.data+"}");
                 temp=temp.next;
             }
 
@@ -61,10 +62,9 @@ public class MyLink {
 
     /**
      * 获取链表的长度
-     * @param head 头指针
      * @return
      */
-    public static int length(Node head){
+    public static int length(){
         int length=0;
         Node temp=head.next;
         while (temp!=null){
@@ -81,11 +81,49 @@ public class MyLink {
      * @param value 要插入的值
      */
     public static void insertNode(Node head,int index,int value){
-        if(index<1||index>length(head)+1){
+        //插入到指定的位置时候，我们就需要认为的给链表的节点记录位置，我们假设头节点为1
+        //插入的时候不能在头节点以前吧,也不能插入的位置大于链表的长度吧  比如链表长度就是10,非要插入的位置为15,这里加1是头指针。
+        if(index>1||index>length()+1){
             System.out.println("插入的位置不合法");
+            return;
+        }
+        int count=1;
+
+        Node newNode=new Node(value);
+
+        Node temp=head;
+
+        while (head.next!=null){
+            if(index==count++){
+                newNode.next=temp.next;
+                temp.next=newNode;
+                return;
+            }
+            temp=temp.next;
+        }
+    }
+
+    /**
+     * 删除节点
+     * @param index
+     */
+    public void deleteByIndex(int index){
+
+        //判断位置是否合法
+        if(index<1||index>length()+1){
+            System.out.println("插入的位置不合法");
+            return;
         }
 
-
+        int count=1;
+         Node temp=head;
+         while (temp.next!=null){
+             if(index==count++){
+                 temp.next=temp.next.next;
+                 return;
+             }
+         }
+         temp=temp.next;
 
     }
 
